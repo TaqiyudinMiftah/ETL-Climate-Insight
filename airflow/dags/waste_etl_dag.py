9843cc2ae6f2697dda942fb29969a244e71a0b51
Taqiyudin <taqiyudinmiftah.01@gmail.com>
2025-11-30 22:23:10 +0700

    update

--- airflow/dags/waste_etl_dag.py
+++ airflow/dags/waste_etl_dag.py
@@ -1,24 +1,21 @@
-from datetime import datetime
-import os
-import sys
-
-# Make repository code importable from within the container
-PROJECT_DIR = "/opt/airflow/ETL-Climate-Insight"
-if PROJECT_DIR not in sys.path:
-	sys.path.insert(0, PROJECT_DIR)
-
 from airflow import DAG
-from airflow.operators.empty import EmptyOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime
 
+default_args = {
+    "owner": "adn",
+    "start_date": datetime(2024, 1, 1)
+}
 
 with DAG(
-	dag_id="waste_etl_dag",
-	description="Placeholder DAG for Waste ETL",
-	start_date=datetime(2025, 1, 1),
-	schedule=None,
-	catchup=False,
-) as dag:
-	start = EmptyOperator(task_id="start")
-	done = EmptyOperator(task_id="done")
+    "climate_etl",
+    default_args=default_args,
+    schedule_interval="@daily",
+    catchup=False
+):
 
-	start >> done
+    run_etl = BashOperator(
+        task_id="run_etl_scripts",
+        bash_command="python /app/src/main_etl.py",
+        cwd="/app"
+    )
